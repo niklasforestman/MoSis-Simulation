0db13acd4f9170963a25d9ab14fa3e97386b2817
J.Kayser <j.kayser@tu-braunschweig.de>
2020-04-21 18:47:11 +0200

    Bibliotheken eingebunden
    ich hab Bibliotheken eingebunden

--- ClickInteraktion.py
+++ ClickInteraktion.py
@@ -1,3 +1,13 @@
-print("hello world")
-print("test")
-‚
+import sys
+import pygame
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pygame.locals import *
+from random import randint
+import time
+import numpy as np
+
+def clickInteraction():
+    print("Es gab ein Event")
+    pass

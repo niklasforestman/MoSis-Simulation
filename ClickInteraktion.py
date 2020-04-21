e54a6d0f3607cd3cd55dc088e456c20dc196956f
TimKayser <timjustuskayser@web.de>
2020-04-21 19:15:08 +0200

    Pseudocode for some structure Ideas

--- ClickInteraktion.py
+++ ClickInteraktion.py
@@ -12,5 +12,14 @@ def clickInteraction():
     print("Es gab ein Event")
     pass
 
+def godMode():
+    #stop the game to run some crazy stuff only a god could possibly do
+    #here has to be a while loop with all ralevant steps inside th godMode
+    while True:
+        #do the relevant stuf
+        if #resume playing is activated
+            break
 
-print("TEST TEST TTTT TESTTEXT")
+
+
+

6527c183b1b279da54aa5b10803f7da088e53537
J.Kayser <j.kayser@tu-braunschweig.de>
2020-04-21 21:01:01 +0200

    Volkszählung
    wenn mit der Maus geklickt wird wird die "population" an die Funktion Wululululu übergeben.
    Dann wird die gesamte population einmal durchgezählt

--- ClickInteraktion.py
+++ ClickInteraktion.py
@@ -11,17 +11,17 @@ import time
 
 
 
-def clickPauseEvent(event):
+def clickPauseEvent(event, population):
     # is activated via the 'P' for PAUSE key
     #    later versions might include a nice big shiny button insode the GUI
     if event.type == KEYDOWN and event.key == K_p:
         print("++++++++++++++++++++++++++++++")
         print( "EngageGodMode")
         print("++++++++++++++++++++++++++++++")
-        godMode()
+        godMode(population)
 
 
-def godMode():
+def godMode(population):
     #stop the game to run some crazy stuff only a god could possibly do
     #here has to be a while loop with all ralevant steps inside th godMode
     print("++++++++++++++++++++++++++++++")
@@ -36,7 +36,11 @@ def godMode():
                 print("GodMode removed")
             if event.type == MOUSEBUTTONDOWN :
                 print(event.pos) #Gibt die Position relaitv zur oberen linken Ecke des Fensters aus
-                wululululu(event.pos)
-def wululululu(pos):
+                wululululu(event.pos, population)
+def wululululu(pos, population):
     print("X = ", pos[0])
     print("y = ", pos[1])
+    durchzählen = 1
+    for person in population:
+        print(durchzählen)
+        durchzählen += 1

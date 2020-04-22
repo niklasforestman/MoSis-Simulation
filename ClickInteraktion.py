c5fa33f0ec0e665684bae18c85f633b737064766
TimKayser <timjustuskayser@web.de>
2020-04-22 18:04:07 +0200

    minor alterations

--- ClickInteraktion.py
+++ ClickInteraktion.py
@@ -21,7 +21,7 @@ def clickPauseEvent(event, population):
         godMode(population)
 
 
-def godMode(population):
+def godMode():
     #stop the game to run some crazy stuff only a god could possibly do
     #here has to be a while loop with all ralevant steps inside th godMode
     print("++++++++++++++++++++++++++++++")
@@ -37,14 +37,16 @@ def godMode(population):
             if event.type == MOUSEBUTTONDOWN :
                 print(event.pos) #Gibt die Position relaitv zur oberen linken Ecke des Fensters aus
                 wululululu(event.pos, population)
+
+
 def wululululu(pos, population):
     print("X = ", pos[0])
     print("y = ", pos[1])
     aoe = 2
-    durchzählen = 1
+    durchzaehlen = 1
     for person in population:
-        durchzählen += 1
+        durchzaehlen += 1
         if abs(person.ps[0] - pos[0]) <= aoe and abs(person.ps[1] - pos[1]) <= aoe:
-            print(durchzählen)
-            print("Du sollst vom Blitz getroffen werden")
+            print(durchzaehlen)
+            print("SPÜRE MEINE MACHT !!!")
             print(person.ps)

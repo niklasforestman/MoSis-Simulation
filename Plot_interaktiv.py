08ac4b5a34d3bf06994e5d03352c92bacfa2fe1b
Unknown <albrecht.pohl@gmail.com>
2020-04-28 20:48:14 +0200

    Plot mit flexiblem Input
    s. Bugfix in Parallelisierung, Funktion sucht sich jetzt die zu löschenden Anteile selbst statt gesetzter Werte

--- Plot_interaktiv.py
+++ Plot_interaktiv.py
@@ -2,26 +2,36 @@ import plotly.graph_objs as go
 import numpy as np
 from plotly.offline import iplot
 
+'''
+Plot Funktion kann jetzt flexibler mit Input arbeiten
+Vorgehen:
+-   Inputs werden auf 0 überprüft (Arrays sind ursprünglich leer und werden im Programm
+    teils vollgeschrieben) - Anteile, die 0 enthalten werden entfernt
+-   Angepasste Arrays von den vier Kategorien werden gebildet
+-   die vier angepassten Arrays bilden die Datengrundlage für den Plot
+-   Plot wird erstellt
+'''
+
 def Plot_interaktiv(people_alive, people_immune, people_infected, people_dead):
 
     alive_end = go.Scatter(
-        x=np.arange(np.where(people_alive == 0)[0][0]),
-        y=people_alive[:np.where(people_alive == 0)[0][0]],
+        x=np.arange(np.nonzero(people_alive)[0][0], np.nonzero(people_alive)[0][-1]+1),
+        y=people_alive[np.nonzero(people_alive)[0][0]:np.nonzero(people_alive)[0][-1]+1],
         name='Alive'
     )
     immune_end = go.Scatter(
-        x=np.arange(np.where(people_alive == 0)[0][0]),
-        y=people_immune[:np.where(people_alive == 0)[0][0]],
+        x=np.arange(np.nonzero(people_alive)[0][0], np.nonzero(people_alive)[0][-1]+1),
+        y=people_immune[np.nonzero(people_alive)[0][0]:np.nonzero(people_alive)[0][-1]+1],
         name='Immune'
     )
     infected_end = go.Scatter(
-        x=np.arange(np.where(people_alive == 0)[0][0]),
-        y=people_infected[:np.where(people_alive == 0)[0][0]],
+        x=np.arange(np.nonzero(people_alive)[0][0], np.nonzero(people_alive)[0][-1]+1),
+        y=people_infected[np.nonzero(people_alive)[0][0]:np.nonzero(people_alive)[0][-1]+1],
         name='Infected'
     )
     deceased_end = go.Scatter(
-        x=np.arange(np.where(people_alive == 0)[0][0]),
-        y=people_dead[:np.where(people_alive == 0)[0][0]],
+        x=np.arange(np.nonzero(people_alive)[0][0], np.nonzero(people_alive)[0][-1]+1),
+        y=people_dead[np.nonzero(people_alive)[0][0]:np.nonzero(people_alive)[0][-1]+1],
         name='Deceased'
     )
     data = [alive_end, immune_end, infected_end, deceased_end]

5e3ac91090ba02d40a5b3f74862b99147748a557
Albrecht <albrecht.pohl@tu-bs.de>
2020-05-18 12:12:16 +0200

    Formatierung Testfunktion

--- Testfunktion_Liveplot.py
+++ Testfunktion_Liveplot.py
@@ -8,7 +8,12 @@ figure()
 def plotfunc():
     y.append(randint(0,50))
     x = range(len(y))
-    plot(x,y)
+    line, = plot(x,y)
+    plt.title('Diagram XY')
+    line.set_label('Label Graph')
+    legend()
+    xlabel('Time')
+    ylabel('Stuff')
 
 for i in range(200):
     drawnow(plotfunc)
